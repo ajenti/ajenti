ff3401eba3a9ff26fbd011abd03d0cd8269ca180
Eugeny Pankov <john.pankov@gmail.com>
2010-03-16 10:13:25 +0200

    Added: TextArea control

--- ajenti/ui.py
+++ ajenti/ui.py
@@ -110,12 +110,12 @@ class SwitchContainer(Container):
 class ScrollContainer(Container):
 	width = 100
 	height = 100
-	
+
 	def dump_HTML(self):
 		return '<div class="ui-el-scrollcontainer" style="width:' + str(self.width) + \
 			'px; height:' + str(self.height) + 'px;">' + Container.dump_HTML(self) + '</div>'
 
-		
+
 class Button(Element):
 	text = ""
 
@@ -522,19 +522,19 @@ class DataTableRow(Element):
 
 class TreeContainer(VContainer):
 	pass
-	
-	
+
+
 class TreeContainerNode(VContainer):
 	text = ""
 	expanded = ""
 	_img = None
-	
+
 	def __init__(self, t=''):
 		VContainer.__init__(self)
 		self.text = t
 		self._img = Image('')
 		self._img.handler = self._on_clicked
-		
+
 	def dump_HTML(self):
 		self._img.file = 'core;ui/tree-minus.png' if self.expanded else 'core;ui/tree-plus.png'
 		s = '<div class="ui-el-treecontainernode"><div class="ui-el-treecontainernode-button"><a href="#">' + self._img.dump_HTML() + '</a></div>' + self.text
@@ -542,14 +542,34 @@ class TreeContainerNode(VContainer):
 			s += '<div class="ui-el-treecontainernode-inner">' + VContainer.dump_HTML(self) + '</div>'
 		s += '</div>'
 		return s
-		
+
 	def handle(self, target, event, data):
 		VContainer.handle(self, target, event, data)
 		self._img.handle(target, event, data)
-		
+
 	def _on_clicked(self, target, event, data):
 		print 'asd'
 		if event == 'click':
 			self.expanded = not self.expanded
 
 
+class TextArea(Element):
+	text = ""
+	width = "auto"
+	height = "auto"
+
+	def __init__(self, t=''):
+		Element.__init__(self)
+		self.text = t
+
+	def dump_HTML(self):
+		s = '<textarea class="ui-el-textarea" onblur="javascript:ajaxNoUpdate(\'/handle;' + self.id + \
+			';update;\'+escape(document.getElementById(\'' + self.id + '\').value));" id="' + \
+			self.id + '"';
+		if self.disabled: s += ' disabled '
+		s += 'style="width:' + str(self.width) + 'px; height:' + str(self.height) + 'px;">' + self.text + "</textarea>";
+		return s
+
+	def handler(self, t, e, d):
+		if e == 'update':
+			self.text = util.url_decode(d)
