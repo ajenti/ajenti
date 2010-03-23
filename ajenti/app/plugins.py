8df4759da533c444aa611c82780f396365e35893
Dmitry Zamaruev <dmitry.zamaruev@gmail.com>
2010-03-23 13:56:36 +0200

    commited missed files

---
+++ ajenti/app/plugins.py
@@ -0,0 +1,20 @@
+import os
+import imp
+import sys
+
+
+def loader(path):
+    plugs = [plug for plug in os.listdir(path) if not plug.startswith('.')]
+    plugs = [plug[:-3] if plug.endswith('.py') else plug for plug in plugs]
+    plugs = list(set(plugs)) # Leave just unique items
+    plugs_found = {}
+    for plug in plugs:
+        try:
+            p = imp.find_module(plug, [path])
+            plugs_found[plug] = p
+        except ImportError:
+            pass
+    for plug in plugs_found:
+        p = plugs_found[plug]
+        imp.load_module(plug, p[0], p[1], p[2])
+
