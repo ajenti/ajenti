f1604bad06e9240c13c0ceab9e6f9e3bb6e849e7
Eugeny Pankov <john.pankov@gmail.com>
2010-06-02 11:07:14 +0300

    Apache plugin - module management

--- plugins/apache/main.py
+++ plugins/apache/main.py
@@ -18,6 +18,8 @@ class Apache(CategoryPlugin):
     def on_session_start(self):
         self._tab = 0
         self._editing_host = ''
+        self._editing_module = ''
+        
         
     def get_ui(self):
         hdr = UI.HContainer(
@@ -56,8 +58,31 @@ class Apache(CategoryPlugin):
             
         phosts = UI.VContainer(th)
         
+        tm = UI.DataTable()
+        hr = UI.DataTableRow(
+                UI.DataTableCell(UI.Label(), width='20px'),
+                UI.DataTableCell(UI.Label(text='Name'), width='200px'),
+                UI.DataTableCell(UI.Label(text='Controls'), width='150px'),
+                header=True
+             )
+        tm.appendChild(hr)
         
-        pmods = UI.VContainer()
+        for h in list_modules():
+            if module_enabled(h):
+                ctl = UI.LinkLabel(text='Disable', id='stopmod/' + h)
+            else: 
+                ctl = UI.LinkLabel(text='Enable', id='startmod/' + h)
+            r = UI.DataTableRow(
+                    UI.Image(file=('/dl/apache/' + ('run.png' if module_enabled(h) else 'stop.png'))),
+                    UI.Label(text=h),
+                    UI.HContainer(
+                        (UI.LinkLabel(text='Edit', id='editmod/' + h) if module_has_config(h) else None),
+                        ctl
+                    )
+                )
+            tm.appendChild(r)
+        
+        pmods = UI.VContainer(tm)
         
         tc = UI.TabControl(active=self._tab)
         tc.add('Hosts', phosts)
@@ -76,16 +101,35 @@ class Apache(CategoryPlugin):
                   )
             p.appendChild(UI.vnode(dlg))
 
+        if self._editing_module != '':
+            dlg = UI.DialogBox(
+                      UI.TextInputArea(name='config', text=read_module_config(self._editing_module).replace('\n', '[br]'), width=800, height=500),
+                      title="Edit module config", id="dlgEditModule", action="/handle/dialog/submit/dlgEditModule"
+                  )
+            p.appendChild(UI.vnode(dlg))
+
         return p
 
     @event('linklabel/click')
     def on_llclick(self, event, params, vars=None):
         if params[0] == 'stophost':
+            self._tab = 0
             disable_host(params[1])
         if params[0] == 'starthost':
+            self._tab = 0
             enable_host(params[1])
         if params[0] == 'edithost':
+            self._tab = 0
             self._editing_host = params[1]
+        if params[0] == 'stopmod':
+            self._tab = 1
+            disable_module(params[1])
+        if params[0] == 'startmod':
+            self._tab = 1
+            enable_module(params[1])
+        if params[0] == 'editmod':
+            self._tab = 1
+            self._editing_module = params[1]
 
     @event('dialog/submit')
     def on_submit(self, event, params, vars=None):
@@ -93,6 +137,10 @@ class Apache(CategoryPlugin):
             if vars.getvalue('action', '') == 'OK':
                 save_host_config(self._editing_host, vars.getvalue('config', ''))
             self._editing_host = '' 
+        if params[0] == 'dlgEditModule':
+            if vars.getvalue('action', '') == 'OK':
+                save_module_config(self._editing_module, vars.getvalue('config', ''))
+            self._editing_module = '' 
     
         
 class ApacheContent(ModuleContent):
