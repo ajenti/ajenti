ef4789bc6f01f077dedfaa819d0e9e408b85bf04
Eugeny Pankov <john.pankov@gmail.com>
2010-08-02 17:09:37 +0300

    SARG support. Test #12.

--- ajenti/utils.py
+++ ajenti/utils.py
@@ -1,5 +1,8 @@
 import subprocess
 import platform
+import os
+import mimetypes
+from datetime import datetime
 
 def dequote(s):
     s = str(s).replace('[br]', '\n').replace('&amp;', '&').replace('&gt;', '>').replace('&lt;', '<')
@@ -45,3 +48,51 @@ def shell_stdin(c, input):
             stdout=subprocess.PIPE,
             stdin=subprocess.PIPE)
     return p.communicate(input)
+    
+def wsgi_serve_file(req, start_response, file):
+    # Check for directory traversal
+    if file.find('..') > -1:
+        start_response('404 Not Found', [])
+        return ''
+            
+    # Check if this is a file
+    if not os.path.isfile(file):
+        start_response('404 Not Found',[])
+        return ''
+
+    headers = []
+    # Check if we have any known file type
+    # For faster response, check for known types:
+    content_type = 'application/octet-stream'
+    if file.endswith('.css'):
+        content_type = 'text/css'
+    elif file.endswith('.js'):
+        content_type = 'application/javascript'
+    elif file.endswith('.png'):
+        content_type = 'image/png'
+    else:
+        (mimetype, encoding) = mimetypes.guess_type(file)
+        if mimetype is not None:
+            content_type = mimetype
+    headers.append(('Content-type',content_type))
+
+    size = os.path.getsize(file)
+    mtimestamp = os.path.getmtime(file)
+    mtime = datetime.utcfromtimestamp(mtimestamp)
+
+    rtime = req.get('HTTP_IF_MODIFIED_SINCE', None)
+    if rtime is not None:
+        try:
+            self.log.debug('Asked for If-Modified-Since: %s'%rtime)
+            rtime = datetime.strptime(rtime, '%a, %b %d %Y %H:%M:%S GMT')
+            if mtime <= rtime:
+                start_response('304 Not Modified',[])
+                return ''
+        except:
+            pass 
+ 
+    headers.append(('Content-length',str(size)))
+    headers.append(('Last-modified',mtime.strftime('%a, %b %d %Y %H:%M:%S GMT')))
+    start_response('200 OK', headers)
+    return req['wsgi.file_wrapper'](open(file))    
+     
