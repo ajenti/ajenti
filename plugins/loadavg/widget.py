7bd36c05d3ee47a98ccf6c9b4ac37f96c3e63763
Eugeny Pankov <john.pankov@gmail.com>
2010-09-25 05:38:30 +0300

    Fix

--- plugins/loadavg/widget.py
+++ plugins/loadavg/widget.py
@@ -56,7 +56,7 @@ class SwapWidget(Plugin):
                 UI.HContainer(
                     UI.Image(file='/dl/loadavg/widget_swap.png'),
                     UI.Label(text='Swap:', bold=True),
-                    UI.ProgressBar(value=ru, max=rt, width=100) if rt != '0' else None,
+                    UI.ProgressBar(value=ru, max=rt, width=100) if int(rt) != 0 else None,
                     UI.Label(text="%sM / %sM"%(ru,rt))
                 )
             )
