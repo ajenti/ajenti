8463e73d2a6739200d3596ceab1d74d68c16469c
Eugeny Pankov <john.pankov@gmail.com>
2010-10-17 16:17:36 +0300

    [networking] fix for option case in rc.conf [packaging] deps for loadavg

--- plugins/network/nc_bsd.py
+++ plugins/network/nc_bsd.py
@@ -39,10 +39,10 @@ class BSDNetworkConfig(BSDIfconfig, ClusteredConfig):
             if not iface.auto:
                 s = s[:-1]
                 
-            if s[0] == 'DHCP':
+            if s[0].upper() == 'DHCP':
                 iface.type = 'inet'
                 iface.addressing = 'dhcp'
-            if s[0] in ['inet', 'inet6']:
+            if s[0].lower() in ['inet', 'inet6']:
                 iface.type = s[0]
                 iface.addressing = 'static'
                 iface.params['address'] = s[1]
