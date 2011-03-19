4a353b01870a5e02aaa4e653b0b4ec92db30f952
Eugeny Pankov <john.pankov@gmail.com>
2011-03-19 19:40:46 +0200

    Fix again

--- ajenti/plugins/dashboard/main.py
+++ ajenti/plugins/dashboard/main.py
@@ -32,7 +32,7 @@ class Dashboard(CategoryPlugin):
         for wgt in self.app.grab_plugins(IDashboardWidget):
             (li if wgt.plugin_id in self._left else ri).append(wgt)
             if not wgt.plugin_id in li:
-                li.append(x.plugin_id)
+                li.append(wgt.plugin_id)
                 
         li = sorted(li, key=lambda x: self._left.index(x.plugin_id))
         ri = sorted(ri, key=lambda x: self._right.index(x.plugin_id))
