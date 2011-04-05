055957ba8c0ebd4e79e9643b425b1f2d71a90fbe
Eugeny Pankov <john.pankov@gmail.com>
2011-04-05 22:47:56 +0300

    [dnsmasq] Reading config

--- plugins/dnsmasq/main.py
+++ plugins/dnsmasq/main.py
@@ -15,9 +15,39 @@ class DnsMasqPlugin(apis.services.ServiceControlPlugin):
 
     def on_init(self):
         self.backend = Backend(self.app) 
+        self.cfg = self.backend.get_config()
         
     def get_main_ui(self):
         ui = self.app.inflate('dnsmasq:main')
+        
+        for lease in self.backend.get_leases():
+            row = UI.DataTableRow(
+                UI.Label(text=lease['mac']),
+                UI.Label(text=lease['ip']),
+                UI.Label(text=lease['host']),
+            )
+            ui.append('list', row)
+
+        for host in self.cfg['dhcp-hosts']:
+            row = UI.DataTableRow(
+                UI.Label(text=self.backend.find_mac(host)),
+                UI.Label(text=self.backend.str_ident(host['id'])),
+                UI.Label(text=self.backend.find_ip(host)),
+                UI.Label(text=self.backend.str_act(host['act'])),
+                UI.HContainer(
+                    UI.MiniButton(
+                        id='editHost/%i' % self.cfg['dhcp-hosts'].index(host),
+                        text='Edit',
+                    ),
+                    UI.WarningMiniButton(
+                        id='deleteHost/%i' % self.cfg['dhcp-hosts'].index(host),
+                        msg='Delete host rule',
+                        text='Delete',
+                    ),
+                )
+            )
+            ui.append('hosts', row)
+            
         return ui
    
     @event('button/click')
