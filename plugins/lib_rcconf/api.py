98f480f5a0d3a51c52658aeb757412d317e921f5
Eugeny Pankov <john.pankov@gmail.com>
2011-07-08 15:23:16 +0200

    Fixed LP801865

--- plugins/lib_rcconf/api.py
+++ plugins/lib_rcconf/api.py
@@ -2,7 +2,6 @@ from ajenti.com import *
 from ajenti.apis import API
 from ajenti import apis
 from ajenti.utils import shell_status, shell
-from ajenti.plugins.recovery import *
 
 
 class RCConf(API):
@@ -10,10 +9,10 @@ class RCConf(API):
         platform = ['arch', 'freebsd', 'centos']
         multi_instance = True
         file = '/etc/rc.conf'
-        
+
         def has_param(self, param):
             return shell_status('grep \'%s=\' %s'%(param,self.file)) == 0
-            
+
         def get_param(self, param):
             s = shell('grep \'^%s=\' %s'%(param,self.file)).split('=')[1].strip()
             return s.strip('"')
@@ -29,13 +28,6 @@ class RCConf(API):
                     done = True
                 if not (('=' in s) and s.split('=')[0].strip() == param):
                     f.write(s + '\n')
-            if not done and param is not None: 
+            if not done and param is not None:
                 f.write('%s="%s"\n'%(param,value))
-            f.close()        
-
-
-class RCConfRecovery(SimpleFileRecoveryProvider):
-    name = 'rc.conf'
-    id = 'rcconf'
-    path = '/etc/rc.conf'
-    
+            f.close()
