e7d4fdd8c84240d33ead1982466602a6234086a9
Eugeny Pankov <john.pankov@gmail.com>
2011-07-14 17:03:18 +0200

    0.5-9, added SSL key support

--- ajenti/standalone.py
+++ ajenti/standalone.py
@@ -15,32 +15,32 @@ import ajenti.utils
 
 
 
-class DebugHandler (logging.NullHandler):
+class DebugHandler (logging.StreamHandler):
     def __init__(self):
         self.capturing = False
         self.buffer = ''
-        
+
     def start(self):
         self.capturing = True
-        
+
     def stop(self):
         self.capturing = False
-        
+
     def handle(self, record):
         if self.capturing:
             self.buffer += self.formatter.format(record) + '\n'
-        
+
 
 def run_server(log_level=logging.INFO, config_file=''):
     # Initialize logging subsystem
     log = logging.getLogger('ajenti')
     log.setLevel(logging.DEBUG)
-    
-    stdout = logging.StreamHandler(stream=sys.stdout)
+
+    stdout = logging.StreamHandler(sys.stdout)
     stdout.setLevel(log_level)
     log.blackbox = DebugHandler()
     log.blackbox.setLevel(logging.DEBUG)
-    
+
     dformatter = logging.Formatter('%(asctime)s %(levelname)-8s %(module)s.%(funcName)s(): %(message)s')
     sformatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
     stdout.setFormatter(dformatter if log_level == logging.DEBUG else sformatter)
@@ -51,7 +51,7 @@ def run_server(log_level=logging.INFO, config_file=''):
 
     # For the debugging purposes
     log.info('Ajenti %s' % version())
-    
+
     # We need this early
     ajenti.utils.logger = log
 
@@ -67,31 +67,31 @@ def run_server(log_level=logging.INFO, config_file=''):
     config.set('log_facility',log)
 
     log.blackbox.start()
-        
+
     # Load external plugins
     load_plugins(config.get('ajenti', 'plugins'), log)
 
 
     # Start components
-    ComponentManager.create(Application(config))    
+    ComponentManager.create(Application(config))
 
     # Start server
     host = config.get('ajenti','bind_host')
     port = config.getint('ajenti','bind_port')
     log.info('Listening on %s:%d'%(host, port))
-    
+
     resource = WSGIResource(
-            reactor, 
+            reactor,
             reactor.getThreadPool(),
             AppDispatcher(config).dispatcher
     )
-    
-    site = server.Site(resource)	
+
+    site = server.Site(resource)
     config.set('server', reactor)
 
     if config.getint('ajenti', 'ssl') == 1:
         ssl_context = ssl.DefaultOpenSSLContextFactory(
-    	    config.get('ajenti','cert_file'), 
+    	    config.get('ajenti','cert_key'),
     	    config.get('ajenti','cert_file')
         )
         reactor.listenSSL(
@@ -108,10 +108,10 @@ def run_server(log_level=logging.INFO, config_file=''):
     reactor.run()
 
     ComponentManager.get().stop()
-    
+
     if hasattr(reactor, 'restart_marker'):
         log.info('Restarting by request')
-        
+
         fd = 20 # Close all descriptors. Creepy thing
         while fd > 2:
             try:
@@ -120,8 +120,7 @@ def run_server(log_level=logging.INFO, config_file=''):
             except:
                 pass
             fd -= 1
-            
+
         os.execv(sys.argv[0], sys.argv)
-    else: 
+    else:
         log.info('Stopped by request')
-        
