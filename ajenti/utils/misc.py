b53d6b2610b25e736ed525b5172472bc38e080f9
Eugeny Pankov <john.pankov@gmail.com>
2011-08-14 12:27:51 +0200

    [utils] fixed BackgroundProcess output reading

--- ajenti/utils/misc.py
+++ ajenti/utils/misc.py
@@ -16,7 +16,7 @@ class BackgroundProcess:
     exitcode = None
     alive = False
     _aborted = False
-    
+
     def __init__(self, cmdline):
         self.thread = threading.Thread(target=self._run, args=[cmdline])
         self.output = ''
@@ -25,33 +25,34 @@ class BackgroundProcess:
         self.cmdline = cmdline
         self.alive = False
         self._aborted = False
-        
+
     def is_running(self):
         return self.alive
-        
+
     def start(self):
         if not self.is_running():
             self.thread.start()
         self.alive = True
-        
+
     def _run(self, c):
         self.process = subprocess.Popen(c, shell=True,
                                            stderr=subprocess.PIPE,
                                            stdout=subprocess.PIPE,
                                            stdin=subprocess.PIPE)
-        
+
         self.output += self.process.stdout.readline() # Workaround; waiting first causes a deadlock
         while self.process.returncode is None and not self._aborted:
             self.output += self.process.stdout.readline()
             self.process.poll()
         self.errors += self.process.stderr.read()
+        self.output += self.process.stdout.read()
         self.exitcode = self.process.returncode
         self.alive = False
-        
+
     def feed_input(self, data):
         if self.is_running():
             self.process.stdin.write(data)
-    
+
     def kill(self):
         self._aborted = True
         if self.is_running():
@@ -72,30 +73,30 @@ class BackgroundWorker:
         self.alive = False
         self.output = ''
         self._aborted = False
-        
+
     def is_running(self):
         if self.alive:
             return not self.thread.killed
         return False
-        
+
     def start(self):
         if not self.is_running():
             self.thread.start()
         self.alive = True
-        
+
     def __run(self, *args):
         self.run(*args)
         self.alive = False
-        
+
     def kill(self):
         self._aborted = True
         if self.is_running():
             self.thread.kill()
-            
-            
+
+
 class KThread(threading.Thread):
     # A killable Thread class
-    
+
     def __init__(self, *args, **keywords):
         threading.Thread.__init__(self, *args, **keywords)
         self.killed = False
@@ -123,5 +124,4 @@ class KThread(threading.Thread):
         return self.localtrace
 
     def kill(self):
-        self.killed = True            
-
+        self.killed = True
--- plugins/terminal/main.py
+++ plugins/terminal/main.py
@@ -29,11 +29,11 @@ class TerminalPlugin(CategoryPlugin, URLHandler):
     def on_session_start(self):
         self._proc = None
         self._inputting = False
-        
+
     def on_init(self):
         if self._proc is None:
             self.start();
-    
+
     def get_ui(self):
         ui = self.app.inflate('terminal:main')
         if self._inputting:
@@ -52,25 +52,25 @@ class TerminalPlugin(CategoryPlugin, URLHandler):
         env['LINES'] = str(TERM_H)
         sh = self.app.get_config(self).shell
         reactor.spawnProcess(
-            self._proc, 
-            shlex.split(sh)[0], 
-            shlex.split(sh), 
-            usePTY=True, 
+            self._proc,
+            shlex.split(sh)[0],
+            shlex.split(sh),
+            usePTY=True,
             env=env
         )
-        
+
     def restart(self):
         if self._proc is not None:
             self._proc.kill()
         self.start()
-        
+
     @url('^/term-get.*$')
     def get(self, req, start_response):
         if self._proc is None:
             self.start();
         if req['PATH_INFO'] == '/term-get-history':
             data = self._proc.history()
-        else:   
+        else:
             data = self._proc.read()
         sio = StringIO.StringIO()
         gz = gzip.GzipFile(fileobj=sio, mode='w')
@@ -83,21 +83,21 @@ class TerminalPlugin(CategoryPlugin, URLHandler):
         data = req['PATH_INFO'].split('/')[2]
         self._proc.write(b64decode(data))
         return ''
-    
+
     @event('button/click')
     def click(self, evt, params, vars):
         if params[0] == 'restart':
             self.restart()
         if params[0] == 'input':
             self._inputting = True
-            
+
     @event('dialog/submit')
     def submit(self, evt, params, vars=None):
         if vars.getvalue('action', None) == 'OK':
             self._proc.write(vars.getvalue('value', ''))
         self._inputting = False
-        
-    
+
+
 class PTYProtocol(protocol.ProcessProtocol):
     def __init__(self):
         self.data = ''
@@ -106,15 +106,15 @@ class PTYProtocol(protocol.ProcessProtocol):
         self.term = pyte.DiffScreen(TERM_W,TERM_H)
         self.stream = pyte.Stream()
         self.stream.attach(self.term)
-        
+
     def connectionMade(self):
         pass
-            
+
     def outReceived(self, data):
         with self.cond:
             with self.lock:
                 self.data += data
-            self.cond.notifyAll()        
+            self.cond.notifyAll()
 
     def read(self):
         with self.cond:
@@ -126,10 +126,10 @@ class PTYProtocol(protocol.ProcessProtocol):
                 if len(data) > 0:
                     self.stream.feed(unicode(str(data)))
         return self.format()
-        
+
     def history(self):
         return self.format(full=True)
-        
+
     def format(self, full=False):
         l = {}
         self.term.dirty.add(self.term.cursor.y)
@@ -143,11 +143,10 @@ class PTYProtocol(protocol.ProcessProtocol):
             'cursor': not self.term.cursor.hidden,
         }
         return r
-        
+
     def write(self, data):
         self.transport.write(data)
 
     def kill(self):
         if self.transport.pid:
             os.kill(self.transport.pid, signal.SIGKILL)
-
