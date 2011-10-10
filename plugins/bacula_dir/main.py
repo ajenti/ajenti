c185ee2ea5722790cb340a655741a65070cbf236
Eugeny Pankov <john.pankov@gmail.com>
2011-10-10 17:46:34 +0300

    Bacula revived!

--- plugins/bacula_dir/main.py
+++ plugins/bacula_dir/main.py
@@ -19,18 +19,61 @@ class BaculaDirPlugin(apis.services.ServiceControlPlugin):
     def get_main_ui(self):
         ui = self.app.inflate('bacula_dir:main')
         st = self.dir.get_status()
-        print st
+
+        for x in st['scheduled']:
+            ui.append('scheduled', UI.DTR(
+                UI.Label(text=x['name']),
+                UI.Label(text=x['level']),
+                UI.Label(text=x['volume']),
+                UI.Label(text=x['date']),
+                UI.Label(text=x['type']),
+                UI.HContainer(
+                    UI.TipIcon(
+                        id='run/%s'%x['name'],
+                        text='Run job',
+                        icon='/dl/core/ui/stock/service-run.png'
+                    ),
+                    UI.TipIcon(
+                        id='edit/%s'%x['name'],
+                        text='Edit job',
+                        icon='/dl/core/ui/stock/edit.png'
+                    ),
+                    UI.TipIcon(
+                        id='delete/%s'%x['name'],
+                        text='Delete job',
+                        icon='/dl/core/ui/stock/delete.png'
+                    ),
+                )
+            ))
+
+        for x in st['running']:
+            ui.append('running', UI.DTR(
+                UI.Label(text=x['id']),
+                UI.Label(text=x['name']),
+                UI.Label(text=x['level']),
+                UI.HContainer(
+                    UI.TipIcon(
+                        id='stop/%s',
+                        text='Cancel job',
+                        icon='/dl/core/ui/stock/service-stop.png'
+                    )
+                )
+            ))
+
+        for x in st['terminated']:
+            ui.append('terminated', UI.DTR(
+                UI.StatusCell(text=x['status'], status='info' if (x['status']=='OK') else 'err'),
+                UI.Label(text=x['name']),
+                UI.Label(text=x['id']),
+                UI.Label(text=x['level']),
+                UI.Label(text=x['files']),
+                UI.Label(text=x['bytes']),
+                UI.Label(text=x['finished']),
+                None
+            ))
         return ui
    
     @event('button/click')
     def on_click(self, event, params, vars=None):
-        if params[0] == 'msgInfo':
-            r = apis.bacula.query('status director')
-            self.put_message('info', r)
-        if params[0] == 'msgWarn':
-            self.put_message('warn', 'Warning')
-        if params[0] == 'msgErr':
-            self.put_message('err', 'Error')
-        if params[0] == 'progress':
-            PlaygroundProgress(self.app).start()
-            
+        if params[0] == 'run':
+            self.dir.run_job(params[1])
