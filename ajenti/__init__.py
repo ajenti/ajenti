5df740790cb4d23018d8303f26bf049fe8609cb7
Eugeny Pankov <john.pankov@gmail.com>
2011-10-31 17:41:00 +0300

    Fixed version bump

--- ajenti/__init__.py
+++ ajenti/__init__.py
@@ -6,7 +6,7 @@ def version():
     :returns: str
     """
 
-    release = '0.6.0'
+    release = '0.6.1'
     p = subprocess.Popen('git describe --tags 2> /dev/null',
             shell=True,
             stdout=subprocess.PIPE)
