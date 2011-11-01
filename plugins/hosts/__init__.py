a496a9ae3a980f3abb5bef839cecdfdb5ef5ff18
Eugeny Pankov <john.pankov@gmail.com>
2011-11-01 15:34:11 +0300

    Added missing rcconf dep

--- plugins/hosts/__init__.py
+++ plugins/hosts/__init__.py
@@ -1,11 +1,14 @@
 MODULES = ['main', 'backend']
 
-DEPS =  []
+DEPS =  [(['freebsd', 'centos', 'arch', 'gentoo'],
+     [
+		('plugin', 'lib_rcconf'),
+     ])]
 
 NAME = 'Hosts'
 PLATFORMS = ['any']
 DESCRIPTION = 'Edit hosts file'
-VERSION = '0'
+VERSION = '3'
 GENERATION = 1
 AUTHOR = 'Ajenti team'
 HOMEPAGE = 'http://ajenti.org'
