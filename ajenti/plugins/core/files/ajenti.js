a068c2d76c1fe2285e141b6b3e467d79781e40ff
Eugeny Pankov <john.pankov@gmail.com>
2011-11-16 20:00:58 +0300

    Changed UserEcho widget to direct link

--- ajenti/plugins/core/files/ajenti.js
+++ ajenti/plugins/core/files/ajenti.js
@@ -68,14 +68,6 @@ Ajenti = {
         Ajenti.query('/handle/nothing');
         Ajenti.Core.requestProgress();
         Ajenti.UI.animateProgress();
-
-        (function() {
-            var _ue = document.createElement('script'); _ue.type = 'text/javascript'; _ue.async = true;
-            _ue.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
-          })();
-
-        setTimeout(1000, "UE.Popin.preload()");
     },
 
     Core: {
@@ -232,11 +224,6 @@ Ajenti = {
             );
             Ajenti.UI._animateProgressTimeout = setTimeout('Ajenti.UI.animateProgress()', 1000);
         },
-
-        showFeedback: function () {
-            UE.Popin.show();
-            return false;
-        }
     }
 };
 
