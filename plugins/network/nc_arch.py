d35c4620ec9a8db09d6263596dade751f8a8abe8
Shanto <shanto@hotmail.com>
2011-12-03 18:02:26 +0600

    Fix Arch network config plugin (for single interface mode only). Fixes https://bugs.launchpad.net/ajenti/+bug/899357 and related.

--- plugins/network/nc_arch.py
+++ plugins/network/nc_arch.py
@@ -5,6 +5,7 @@ from ajenti import apis
 from api import *
 from nctp_linux import *
 
+rc_net_keys = ('address', 'netmask', 'broadcast', 'gateway')
 
 class ArchNetworkConfig(LinuxIfconfig):
     implements(INetworkConfig)
@@ -12,50 +13,37 @@ class ArchNetworkConfig(LinuxIfconfig):
     
     interfaces = None
     
-    
     def __init__(self):
         self.rcconf = apis.rcconf.RCConf(self.app)
         self.rescan()
-
+    
     def rescan(self):
         self.interfaces = {}
-        for name in self.rcconf.get_param('INTERFACES')[1:-1].split(','):
-            try:
-                s = self.rcconf.get_param(name).split()
-            except:
-                continue
-            if not s:
-                continue
-            iface = NetworkInterface()
-            iface.name = name
-            self.interfaces[name] = iface
-            if s[0] == 'dhcp':
-                iface.type = 'inet'
-                iface.addressing = 'dhcp'
-            else:
-                iface.type = 'inet'
-                iface.addressing = 'static'
-                iface.params['address'] = s[1]
-                s = s[2:]
-                while len(s)>0:
-                    iface.params[s[0]] = s[1]
-                    s = s[2:]
-                    
-            iface.devclass = self.detect_dev_class(iface)
-            iface.up = shell_status('ifconfig ' + iface.name + '|grep UP') == 0
-            iface.get_bits(self.app, self.detect_iface_bits(iface))
-
-
+        name = self.rcconf.get_param('interface')
+        
+        if name == '':
+            return
+        
+        iface = NetworkInterface()
+        iface.name = name
+        iface.type = 'inet'
+        iface.auto = True
+        self.interfaces[name] = iface
+        
+        for key in rc_net_keys:
+            value = self.rcconf.get_param(key)
+            if key == 'address':
+                iface.addressing = 'dhcp' if value == '' else 'static'
+            iface.params[key] = value
+        
+        iface.devclass = self.detect_dev_class(iface)
+        iface.up = shell_status('ifconfig ' + iface.name + '|grep UP') == 0
+        iface.get_bits(self.app, self.detect_iface_bits(iface))
+    
     def save(self):
         for iface in self.interfaces.values():
-            if iface.addressing == 'dhcp':
-                self.rcconf.set_param(iface.name, 'dhcp')
-            else:                
-                s = [iface.name, iface.params['address']]
-                for k in iface.params:
-                    if k != 'address':
-                        s.append(k)
-                        s.append(iface.params[k])
-                self.rcconf.set_param(iface.name, ' '.join(s))
-
-  
+            for key in rc_net_keys:
+                value = iface.params[key]
+                if iface.addressing == 'dhcp':
+                    value = ''
+                self.rcconf.set_param(key, value, near='interface')
