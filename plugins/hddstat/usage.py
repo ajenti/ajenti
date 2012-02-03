f01e76b0eb9e0a820e554699a6e0712bdb1720aa
Pablo Albornoz <pablo.albornoz.n@gmail.com>
2012-02-03 15:37:15 -0300

    Disk Usage plugin ready for merging

--- plugins/hddstat/usage.py
+++ plugins/hddstat/usage.py
@@ -1,21 +1,20 @@
 from ajenti.api import *
 from ajenti.utils import shell
 
-class UsageMeter(LinearMeter):
-	name = 'Disk usage'
-	category = 'System'
-	transform = 'percent'
-	
-	def get_usage(self):
-		u = int(shell('df --total | grep total').split().pop().strip('%'))
-		print 'Usage: ', u
-		return u
-	
-	def get_value(self):
-		return self.get_usage()
-	
-	def get_min(self):
-		return 0
-	
-	def get_max(self):
-		return 100
+class DiskUsageMeter(LinearMeter):
+    name = 'Disk usage'
+    category = 'System'
+    transform = 'percent'
+    
+    def get_usage(self):
+        u = int(shell('df --total | grep total').split().pop().strip('%'))
+        return u
+    
+    def get_value(self):
+        return self.get_usage()
+    
+    def get_min(self):
+        return 0
+    
+    def get_max(self):
+        return 100
--- plugins/hddstat/widget.py
+++ plugins/hddstat/widget.py
@@ -4,29 +4,27 @@ from ajenti.com import implements, Plugin
 from ajenti.api import *
 from ajenti.utils import *
 from ajenti import apis
-from usage import UsageMeter
+from usage import DiskUsageMeter
 
-class StatWidget(Plugin):
-	implements(apis.dashboard.IWidget)
-	title = 'HDD Capacity'
-	icon = '/dl/hddstat/icon.png'
-	name = 'Capacity'
-	style = 'normal'
+class DiskUsageWidget(Plugin):
+    implements(apis.dashboard.IWidget)
+    title = 'Disk Usage'
+    icon = '/dl/hddstat/icon.png'
+    name = 'Disk Usage'
+    style = 'normal'
 
-	def get_ui(self, cfg, id=None):
-		m = UsageMeter(self.app).prepare(cfg)
-		#return UI.Label(text=str(m.get_value())+'%')
-		#return UI.ProgressBar(value=m.get_value(), max=m.get_max(), width=220)
-		return UI.HContainer(
-			UI.ProgressBar(value=m.get_value(), max=m.get_max(), width=220),
-			UI.Label(text=str(m.get_value())+'%'),
-		)
+    def get_ui(self, cfg, id=None):
+        m = UsageMeter(self.app).prepare(cfg)
+        return UI.HContainer(
+            UI.ProgressBar(value=m.get_value(), max=m.get_max(), width=220),
+            UI.Label(text=str(m.get_value())+'%'),
+        )
 
-	def handle(self, event, params, cfg, vars=None):
-		pass
+    def handle(self, event, params, cfg, vars=None):
+        pass
 
-	def get_config_dialog(self):
-		return None
-	
-	def process_config(self, vars):
-		pass
+    def get_config_dialog(self):
+        return None
+    
+    def process_config(self, vars):
+        pass
