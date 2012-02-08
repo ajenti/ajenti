3780f6996b1d7a5afa2fd84af22bf8fb3129ee29
egelmex <ellism88@gmail.com>
2012-02-08 19:57:39 +0000

    Make the network plugin work with newer versions of arch linux.
    ifconfig has been depricated, and is not installed by default on arch
    any more.
    This patch makes network plugin use "ip" on arch.
    It also makes it work with arch users who do not set up their network
    with rc.conf (i.e. network manger).

--- plugins/network/nc_arch.py
+++ plugins/network/nc_arch.py
@@ -3,11 +3,11 @@ from ajenti.utils import *
 from ajenti import apis
 
 from api import *
-from nctp_linux import *
+from nctp_ip import *
 
 rc_net_keys = ('address', 'netmask', 'broadcast', 'gateway')
 
-class ArchNetworkConfig(LinuxIfconfig):
+class ArchNetworkConfig(LinuxIp):
     implements(INetworkConfig)
     platform = ['Arch']
     
@@ -30,16 +30,32 @@ class ArchNetworkConfig(LinuxIfconfig):
         iface.auto = True
         self.interfaces[name] = iface
         
-        for key in rc_net_keys:
-            value = self.rcconf.get_param(key)
-            if key == 'address':
-                iface.addressing = 'dhcp' if value == '' else 'static'
-            iface.params[key] = value
+	if self.rcconf.has_param('INTERFACES'):
+          for key in rc_net_keys:
+              value = self.rcconf.get_param(key)
+              if key == 'address':
+                  iface.addressing = 'dhcp' if value == '' else 'static'
+              iface.params[key] = value
         
-        iface.devclass = self.detect_dev_class(iface)
-        iface.up = shell_status('ifconfig ' + iface.name + '|grep UP') == 0
-        iface.get_bits(self.app, self.detect_iface_bits(iface))
-    
+          iface.devclass = self.detect_dev_class(iface)
+          iface.up = shell_status('ifconfig ' + iface.name + '|grep UP') == 0
+          iface.get_bits(self.app, self.detect_iface_bits(iface))
+	else:
+	  s = shell('ip -o link list')
+
+	  for line in s.split('\n'):
+	    line = line.strip()
+	    if line != '':
+ 	      name = line.split(':')[1].strip()
+              iface = NetworkInterface()
+	      iface.name = name
+	      self.interfaces[name] = iface
+	      iface.devclass = self.detect_dev_class(iface)
+	      iface.up = (line.find('state UP') != -1)
+	      iface.get_bits(self.app, self.detect_iface_bits(iface))
+	      iface.editable = False
+   
+
     def save(self):
         for iface in self.interfaces.values():
             for key in rc_net_keys:
