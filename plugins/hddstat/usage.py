dfbbb96d3fa71ab6d913ad306c8b53aced84b693
egelmex <ellism88@gmail.com>
2012-02-13 16:52:46 +0000

    adding support for hdd stat on indervidual disks

--- plugins/hddstat/usage.py
+++ plugins/hddstat/usage.py
@@ -5,16 +5,23 @@ class DiskUsageMeter(LinearMeter):
     name = 'Disk usage'
     category = 'System'
     transform = 'percent'
-    
-    def get_usage(self):
-        u = int(shell('df --total | grep total').split().pop().strip('%'))
+   
+    def get_usage(self, dev):
+        u = shell('df --total | grep -w %s' % dev)
+	print "************** %s " % u
+        if dev == 'total':
+            u = int(u.split().pop().strip('%'))
+        else:
+            u = int(u.split().pop(-2).strip('%'))
         return u
     
-    def get_value(self):
-        return self.get_usage()
+    def get_value(self, dev):
+        return self.get_usage(dev)
     
     def get_min(self):
         return 0
     
     def get_max(self):
         return 100
+
+
