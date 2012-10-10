d78a8f5d4aeff22648aecc1c485766d2973e8f73
Eugeny Pankov <e@ajenti.org>
2012-10-10 10:36:18 +0300

    Fstab autocomplete

---
+++ ajenti/plugins/fstab/disks.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+
+def list_devices(by_name=True, by_uuid=False, by_id=False, by_label=False):
+    result = []
+
+    def add_dir(path, namefx=lambda s, r: s, valuefx=lambda s, r: r):
+        for s in os.listdir(path):
+            rp = os.path.realpath(os.path.join(path, s))
+            result.append((namefx(s, rp), valuefx(s, rp)))
+
+    for s in os.listdir('/dev'):
+        if re.match('sd.$|hd.$|scd.$|fd.$|ad.+$', s):
+            result.append((s, '/dev/' + s))
+
+    if by_uuid:
+        add_dir('/dev/disk/by-uuid', lambda s, r: '%s: UUID %s' % (r, s), lambda s, r: 'UUID=%s' % s)
+
+    if by_label:
+        add_dir('/dev/disk/by-label', lambda s, r: 'Label "%s"' % s, lambda s, r: 'LABEL=%s' % s)
+
+    if by_id:
+        add_dir('/dev/disk/by-id', lambda s, r: '%s: %s' % (r, s))
+
+    return sorted(result, key=lambda x: x[0])
--- ajenti/plugins/fstab/fstab.py
+++ ajenti/plugins/fstab/fstab.py
@@ -5,6 +5,8 @@ from ajenti.ui.binder import Binder
 from reconfigure.configs import FSTabConfig
 from reconfigure.items.fstab import Filesystem
 
+import disks
+
 
 @plugin
 class Filesystems (SectionPlugin):
@@ -12,16 +14,23 @@ class Filesystems (SectionPlugin):
         self.title = 'Filesystems'
         self.append(self.ui.inflate('fstab:main'))
 
+        self.reload_disks()
+        self.find('type').items = ['Auto', 'EXT2', 'EXT3', 'EXT4', 'NTFS', 'FAT', 'ZFS', 'ReiserFS', 'None', 'Loop']
+        self.find('type').values = ['auto', 'ext2', 'ext3', 'ext4', 'ntfs', 'vfat', 'zfs', 'reiser', 'none', 'loop']
+        
         self.config = FSTabConfig(path='/etc/fstab')
         self.config.load()
         self.binder = Binder(self.config.tree, self.find('fstab-config'))
         self.find('filesystems').new_item = lambda c: Filesystem()
-        self.find('device').items = ['1', '2', '3']
-        self.find('device').values = ['a', 'b', 'c']
         self.binder.autodiscover()
         self.binder.populate()
 
         self.find('save').on('click', self.save)
+
+    def reload_disks(self):
+        lst = disks.list_devices(by_uuid=True, by_id=True, by_label=True)
+        self.find('device').items = [x[0] for x in lst]
+        self.find('device').values = [x[1] for x in lst]
 
     def save(self):
         self.binder.update()
