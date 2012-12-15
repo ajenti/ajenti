d876e33f0008132e222caefecaaccf4b17cad209
blackhawkh <blackhawkh@gmail.com>
2012-12-15 19:07:25 +0200

    Update plugins/cpu/cpu.py

--- plugins/cpu/cpu.py
+++ plugins/cpu/cpu.py
@@ -7,10 +7,23 @@ class Cpu(LinearMeter):
     transform = 'percent'
     
     def get_usage(self):
-         u = shell('ps h -eo pcpu').split()
+         if detect_platform() == 'freebsd':
+             u = shell('ps h -xeo pcpu').split()
+         else:
+             u = shell('ps h -eo pcpu').split()
          b=0.0
-         for a in u:  
-            b += float(a)
+         for a in u:
+            try:
+                b += float(a)
+            except:
+                pass
+         if detect_platform() == 'freebsd':
+             ncpus = shell('sysctl -n kern.smp.cpus')
+             if not ncpus.split()[0].isdigit():
+                 ncpus = 1
+             if ncpus == '0' or ncpus < 0:
+                 ncpus = 1
+             b = b/float(ncpus)
          return b
     
     def get_value(self):
