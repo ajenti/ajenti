18251d4d2140814434ffa0cdc05b4302684781d2
Eugeny Pankov <e@ajenti.org>
2013-01-07 13:23:11 +0300

    swap widget

--- ajenti/plugins/sensors/memory.py
+++ ajenti/plugins/sensors/memory.py
@@ -22,6 +22,21 @@ class MemorySensor (Sensor):
 
 
 @plugin
+class SwapSensor (Sensor):
+    id = 'swap'
+    timeout = 5
+
+    def measure(self, variant):
+        used = total = 0
+        for l in open('/proc/swaps').read().split('\n')[2:]:
+            l = l.split()
+            if len(l) > 3:
+                total += int(l[2]) * 1024
+                used += int(l[3]) * 1024
+        return (used, total)
+
+
+@plugin
 class MemoryWidget (DashboardWidget):
     name = 'Memory usage'
     icon = 'tasks'
@@ -34,3 +49,22 @@ class MemoryWidget (DashboardWidget):
         value = self.sensor.value()
         self.find('value').text = str_fsize(value[0])
         self.find('progress').value = 1.0 * value[0] / value[1]
+
+
+@plugin
+class SwapWidget (DashboardWidget):
+    name = 'Swap usage'
+    icon = 'hdd'
+
+    def init(self):
+        self.sensor = Sensor.find('swap')
+        self.append(self.ui.inflate('sensors:progressbar-widget'))
+        self.find('icon').icon = 'hdd'
+        self.find('name').text = 'Swap usage'
+        value = self.sensor.value()
+        self.find('value').text = str_fsize(value[0])
+        if value[1] > 0:
+            frac = 1.0 * value[0] / value[1]
+        else:
+            frac = 0
+        self.find('progress').value = frac
