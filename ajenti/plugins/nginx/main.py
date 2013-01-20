0d399a3cd44221c0a6be0be94a4b16298d2202f4
Eugeny Pankov <e@ajenti.org>
2013-01-20 13:05:37 +0300

    nginx template

--- ajenti/plugins/nginx/main.py
+++ ajenti/plugins/nginx/main.py
@@ -15,6 +15,25 @@ class Nginx (WebserverPlugin):
     hosts_available_dir = '/etc/nginx/sites-available'
     hosts_enabled_dir = '/etc/nginx/sites-enabled'
 
+    template = """server {
+    server_name name;
+    access_log /var/log/nginx/name.access.log;
+    error_log  /var/log/nginx/name.error.log;
+
+    listen 80;
+
+    location / {
+        root /var/www/name;
+    }
+
+    location ~ \.lang$ {
+        include fastcgi_params;
+        fastcgi_pass 127.0.0.1:port;
+        fastcgi_split_path_info ^()(.*)$;
+    }
+}
+"""
+
     def init(self):
         self.title = 'nginx'
         self.category = 'Software'
