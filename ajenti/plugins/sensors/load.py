fe4e5bb0bc132e0d3f091c982ec4b097af371e84
Eugeny Pankov <john.pankov@gmail.com>
2013-02-17 20:20:38 +0300

    [bsd] load average

--- ajenti/plugins/sensors/load.py
+++ ajenti/plugins/sensors/load.py
@@ -1,27 +1,40 @@
+import subprocess
+
 from ajenti.api import plugin
 from ajenti.api.sensors import Sensor
 from ajenti.plugins.dashboard.api import ConfigurableWidget
 
 
-@plugin
-class LoadSensor (Sensor):
-    platforms = ['debian', 'centos']
-
+class BaseLoadSensor (Sensor):
     id = 'load'
     timeout = 1
-
-    def measure(self, variant):
-        idx = self.get_variants().index(variant)
-        return float(open('/proc/loadavg').read().split()[idx])
 
     def get_variants(self):
         return ['1 min', '5 min', '15 min']
 
 
 @plugin
-class LoadWidget (ConfigurableWidget):
+class LinuxLoadSensor (BaseLoadSensor):
     platforms = ['debian', 'centos']
 
+    def measure(self, variant):
+        idx = self.get_variants().index(variant)
+        return float(open('/proc/loadavg').read().split()[idx])
+
+
+@plugin
+class BSDLoadSensor (BaseLoadSensor):
+    platforms = ['freebsd']
+
+    def measure(self, variant):
+        idx = self.get_variants().index(variant)
+        tokens = subprocess.check_output(['uptime']).split()
+        loads = [float(x.strip(',')) for x in tokens[-3:]]
+        return loads[idx]
+
+
+@plugin
+class LoadWidget (ConfigurableWidget):
     name = 'Load average'
     icon = 'signal'
 
