e63058133938b5f3ef1c85409b7cd746e2842cba
Eugene Pankov <john.pankov@gmail.com>
2013-09-17 14:37:51 +0300

    [sensors] BSD uptime (Fixes LP1225768)

--- ajenti/plugins/sensors/uptime.py
+++ ajenti/plugins/sensors/uptime.py
@@ -1,3 +1,7 @@
+import ctypes
+import struct
+import time
+
 from ajenti.api import plugin
 from ajenti.api.sensors import Sensor
 from ajenti.plugins.dashboard.api import DashboardWidget
@@ -5,12 +9,64 @@ from ajenti.util import str_timedelta
 
 
 @plugin
-class UptimeSensor (Sensor):
+class LinuxUptimeSensor (Sensor):
     id = 'uptime'
+    platforms = ['debian', 'centos']
     timeout = 1
 
     def measure(self, variant):
         return float(open('/proc/uptime').read().split()[0])
+
+
+@plugin
+class BSDUptimeSensor (Sensor):
+    id = 'uptime'
+    platforms = ['freebsd']
+    timeout = 1
+
+    def measure(self):
+        """
+        by Koen Crolla
+        https://github.com/Cairnarvon/uptime/blob/master/src/__init__.py
+        """
+
+        try:
+            libc = ctypes.CDLL('libc.so')
+        except AttributeError:
+            return None
+        except OSError:
+            # OS X; can't use ctypes.util.find_library because that creates
+            # a new process on Linux, which is undesirable.
+            try:
+                libc = ctypes.CDLL('libc.dylib')
+            except OSError:
+                return None
+
+        if not hasattr(libc, 'sysctlbyname'):
+            # Not BSD.
+            return None
+
+        # Determine how much space we need for the response.
+        sz = ctypes.c_uint(0)
+        libc.sysctlbyname('kern.boottime', None, ctypes.byref(sz), None, 0)
+        if sz.value != struct.calcsize('@LL'):
+            # Unexpected, let's give up.
+            return None
+
+        # For real now.
+        buf = ctypes.create_string_buffer(sz.value)
+        libc.sysctlbyname('kern.boottime', buf, ctypes.byref(sz), None, 0)
+        sec, usec = struct.unpack('@LL', buf.raw)
+
+        # OS X disagrees what that second value is.
+        if usec > 1000000:
+            usec = 0.
+
+        __boottime = sec + usec / 1000000.
+        up = time.time() - __boottime
+        if up < 0:
+            up = None
+        return up
 
 
 @plugin
