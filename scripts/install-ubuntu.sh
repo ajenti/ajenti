ff34f7af4a5995b9c9ab3d9718c5845c0154858f
Eugene Pankov <john.pankov@gmail.com>
2014-02-13 22:41:15 +0300

    install scripts

---
+++ scripts/install-ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo ':: Installing repo key'
+wget http://repo.ajenti.org/debian/key -O- | apt-key add -
+
+echo ':: Adding repo entry'
+echo "deb http://repo.ajenti.org/debian main main ubuntu" >> /etc/apt/sources.list.d/ajenti.list
+
+echo ':: Updating lists'
+apt-get update
+
+echo ':: Installing package'
+apt-get install ajenti
+
+echo ':: Done! Open https://<address>:8000 in browser'
