cec7369ed322eeacf17c54bfc2c837a16db07f54
Eugene Pankov <john.pankov@gmail.com>
2014-03-08 20:48:09 +0300

    [sensors] new uptime sensor (fixes #284)

--- ajenti/plugins/sensors/uptime.py
+++ ajenti/plugins/sensors/uptime.py
@@ -1,5 +1,4 @@
-import ctypes
-import struct
+import psutil
 import time
 
 from ajenti.api import plugin
@@ -9,64 +8,12 @@ from ajenti.util import str_timedelta
 
 
 @plugin
-class LinuxUptimeSensor (Sensor):
+class UnixUptimeSensor (Sensor):
     id = 'uptime'
-    platforms = ['debian', 'centos', 'arch']
     timeout = 1
 
     def measure(self, variant):
-        return float(open('/proc/uptime').read().split()[0])
-
-
-@plugin
-class BSDUptimeSensor (Sensor):
-    id = 'uptime'
-    platforms = ['freebsd']
-    timeout = 1
-
-    def measure(self, variant):
-        """
-        by Koen Crolla
-        https://github.com/Cairnarvon/uptime/blob/master/src/__init__.py
-        """
-
-        try:
-            libc = ctypes.CDLL('libc.so')
-        except AttributeError:
-            return None
-        except OSError:
-            # OS X; can't use ctypes.util.find_library because that creates
-            # a new process on Linux, which is undesirable.
-            try:
-                libc = ctypes.CDLL('libc.dylib')
-            except OSError:
-                return None
-
-        if not hasattr(libc, 'sysctlbyname'):
-            # Not BSD.
-            return None
-
-        # Determine how much space we need for the response.
-        sz = ctypes.c_uint(0)
-        libc.sysctlbyname('kern.boottime', None, ctypes.byref(sz), None, 0)
-        if sz.value != struct.calcsize('@LL'):
-            # Unexpected, let's give up.
-            return None
-
-        # For real now.
-        buf = ctypes.create_string_buffer(sz.value)
-        libc.sysctlbyname('kern.boottime', buf, ctypes.byref(sz), None, 0)
-        sec, usec = struct.unpack('@LL', buf.raw)
-
-        # OS X disagrees what that second value is.
-        if usec > 1000000:
-            usec = 0.
-
-        __boottime = sec + usec / 1000000.
-        up = time.time() - __boottime
-        if up < 0:
-            up = None
-        return up
+        return time.time() - psutil.BOOT_TIME
 
 
 @plugin
