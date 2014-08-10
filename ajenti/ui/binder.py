3071fd0d902c8951065833ad2b7b0d7f4e2b8ab1
Eugene Pankov <john.pankov@gmail.com>
2014-08-10 22:14:28 +0400

    handle invalid order data

--- ajenti/ui/binder.py
+++ ajenti/ui/binder.py
@@ -440,27 +440,33 @@ class CollectionAutoBinding (Binding):
                 if e.visible:
                     sortable_indexes.append(i)
 
-            absolute_order = [sortable_indexes[i - 1] for i in self.items_ui_element.order]
-            new_indexes = []
-            absolute_order_idx = 0
-            for i in range(len(self.values)):
-                if i in sortable_indexes:
-                    new_indexes.append(absolute_order[absolute_order_idx])
-                    absolute_order_idx += 1
-                else:
-                    new_indexes.append(i)
-            
-            shuffle = lambda a: dict([(old, a[i]) for old, i in enumerate(new_indexes) if i < len(self.collection)])
-            self.binders = shuffle(self.binders)
-            self.item_ui = shuffle(self.item_ui)
+            try:
+                absolute_order = [sortable_indexes[i - 1] for i in self.items_ui_element.order]
+                indexes_valid = True
+            except IndexError:
+                indexes_valid = False
 
-            new_values = [self.values[i] for i in new_indexes if i < len(self.collection)]
-            while len(self.collection) > 0:
-                self.collection.pop(0)
-            for e in new_values:
-                self.collection.append(e)
+            if indexes_valid:
+                new_indexes = []
+                absolute_order_idx = 0
+                for i in range(len(self.values)):
+                    if i in sortable_indexes:
+                        new_indexes.append(absolute_order[absolute_order_idx])
+                        absolute_order_idx += 1
+                    else:
+                        new_indexes.append(i)
+                
+                shuffle = lambda a: dict([(old, a[i]) for old, i in enumerate(new_indexes) if i < len(self.collection)])
+                self.binders = shuffle(self.binders)
+                self.item_ui = shuffle(self.item_ui)
 
-            self.items_ui_element.order = []
+                new_values = [self.values[i] for i in new_indexes if i < len(self.collection)]
+                while len(self.collection) > 0:
+                    self.collection.pop(0)
+                for e in new_values:
+                    self.collection.append(e)
+
+                self.items_ui_element.order = []
 
         for index, value in enumerate(self.values):
             if self.ui.filter(value):
