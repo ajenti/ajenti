a0283b68a5aa0a383a4f3755f46ae7c5a3cd77d6
Eugene Pankov <john.pankov@gmail.com>
2015-03-21 15:30:14 +0300

    travis

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
-echo -en 'travis_fold:start:Python modules\r'
+echo -en 'travis_fold:start:deps.python\r'
+    echo Installing Python modules
     sudo apt-get install -y python-lxml python-dbus python-lxml
     pip install -r ajenti-core/requirements.txt
     pip install -r plugins/core/requirements.txt
     pip install -r plugins/services/requirements.txt
     pip install -r plugins/terminal/requirements.txt
     pip install ajenti-dev-multitool
-echo -en 'travis_fold:end:Python modules\r'
+echo -en 'travis_fold:end:deps.python\r'
 
-echo -en 'travis_fold:start:Node modules\r'
+echo -en 'travis_fold:start:deps.node\r'
+    echo Installing NodeJS modules
     npm install -g bower
     cd tests-karma
     npm install || exit 1
     cd ..
-echo -en 'travis_fold:end:Node modules\r'
+echo -en 'travis_fold:end:deps.node\r'
 
-echo -en 'travis_fold:start:Bower components\r'
+echo -en 'travis_fold:start:deps.bower\r'
+    echo Installing Bower components
     ajenti-dev-multitool --bower install || exit 1
-echo -en 'travis_fold:end:Bower components\r'
+echo -en 'travis_fold:end:deps.bower\r'
 
-echo -en 'travis_fold:start:Nose unit tests\r'
+echo -en 'travis_fold:start:test.nose\r'
+    echo Running Nose tests
     cd tests-nose
     nosetests base.py || exit 1
     cd ..
-echo -en 'travis_fold:end:Nose unit tests\r'
+echo -en 'travis_fold:end:test.nose\r'
 
-echo -en 'travis_fold:start:Karma unit tests\r'
+echo -en 'travis_fold:start:test.karma\r'
+    echo Running Karma tests
     cd tests-karma
     ./node_modules/.bin/karma start karma.conf.coffee --single-run || exit 1
     cat coverage/coverage.txt
     cd ..
-echo -en 'travis_fold:end:Karma unit tests\r'
+echo -en 'travis_fold:end:test.karma\r'
