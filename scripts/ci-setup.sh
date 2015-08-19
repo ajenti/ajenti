8c5addb267a3098dfb0af7662f4bfd7ab2099983
Eugene Pankov <john.pankov@gmail.com>
2015-08-19 13:20:17 +0300

    CI caching

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -en 'travis_fold:start:deps.apt\r'
     echo Installing packages
-    sudo apt-get install -y python-lxml python-dbus python-lxml python-augeas
+    #sudo apt-get install -y python-lxml python-dbus python-lxml python-augeas
 echo -en 'travis_fold:end:deps.apt\r'
