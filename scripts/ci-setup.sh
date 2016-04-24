0cccc47cfa1d09ca4c96b5898aef2120b1147932
Eugene Pankov <john.pankov@gmail.com>
2016-04-24 18:21:28 +0200

    travis fix

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-rm ~/.ssh/id_rsa
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 echo -en 'travis_fold:start:deps.apt\r'
     echo Installing packages
     #sudo apt-get install -y python-lxml python-dbus python-lxml python-augeas
