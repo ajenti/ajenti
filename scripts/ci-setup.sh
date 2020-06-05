8bc5c067382d30ed9adea385257f4e64f84bfca1
kiarn <arnaud@kientz.me>
2020-06-05 18:49:02 +0200

    Update config setup for travis.

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo -en 'travis_fold:start:deps.apt\r'
     echo Installing packages
+    sudo python3 -m pip install wheel setuptools
     #sudo apt-get install -y python-lxml python-dbus python-lxml python-augeas
 echo -en 'travis_fold:end:deps.apt\r'
