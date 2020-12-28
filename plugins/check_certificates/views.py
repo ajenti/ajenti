851e50c11bc7f90323546fa6c798e3cb26050a98
kiarn <arnaud@kientz.me>
2020-12-28 11:09:17 +0100

    Docstrings check_cert.

--- plugins/check_certificates/views.py
+++ plugins/check_certificates/views.py
@@ -1,3 +1,7 @@
+"""
+Module to check if some certificates are still valid or not.
+"""
+
 from jadi import component
 
 from aj.api.http import url, HttpPlugin
@@ -14,7 +18,19 @@ class Handler(HttpPlugin):
     @url(r'/api/check_cert/test')
     @endpoint(api=True)
     def handle_api_check_cert(self, http_context):
+        """
+        Connector between js and api to check the certification from one domain.
+        The url given in post method is in the form www.domain.com:999.
+        All urls are stored in the user config.
+        Method POST.
+
+        :param http_context: HttpContext
+        :type http_context: HttpContext
+        :return: All details from a certificate
+        :rtype: json
+        """
+
         if http_context.method == 'POST':
             url = http_context.json_body()['url']
             return json.loads(json.dumps(checkOnDom(*url.split(':'))))
-            
+
