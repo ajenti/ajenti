24b829828caf3ce7d774a55d3e365343df74fce7
Eugeny <e@ajenti.org>
2021-08-03 09:34:06 +0200

    Update install-rhel7.sh

--- scripts/install-rhel7.sh
+++ scripts/install-rhel7.sh
@@ -5,10 +5,10 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo ':: Adding EPEL repo'
-rpm -ivh http://download.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-7-1.noarch.rpm || true
+rpm -ivh https://download.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-7-1.noarch.rpm || true
 
 echo ':: Adding Ajenti repo'
-rpm -ivh http://repo.ajenti.org/ajenti-repo-1.0-1.noarch.rpm
+rpm -ivh https://repo.ajenti.org/ajenti-repo-1.0-1.noarch.rpm
 
 echo ':: Installing package'
 yum install ajenti -y
