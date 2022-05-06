0dbeb0708a8c997601f3712e78c3852a44c8299f
kiarn <arnaud@kientz.me>
2022-05-06 08:35:11 +0200

    F-strings.

--- ajenti-core/aj/util/strings.py
+++ ajenti-core/aj/util/strings.py
@@ -3,17 +3,17 @@ def str_fsize(sz):
     Formats file size as string (i.e., 1.2 Mb)
     """
     if sz < 1024:
-        return '%.1f bytes' % sz
+        return f'{sz:.1f} bytes'
     sz /= 1024.0
     if sz < 1024:
-        return '%.1f KB' % sz
+        return f'{sz:.1f} KB'
     sz /= 1024.0
     if sz < 1024:
-        return '%.1f MB' % sz
+        return f'{sz:.1f} MB'
     sz /= 1024.0
     if sz < 1024:
-        return '%.1f GB' % sz
+        return f'{sz:.1f} GB'
     sz /= 1024.0
-    return '%.1f TB' % sz
+    return f'{sz:.1f} TB'
 
 
