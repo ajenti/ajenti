b522ac29f9d464e38ae7c46f5da1558b8b870930
kiarn <arnaud@kientz.me>
2022-10-04 12:45:43 +0200

    Convert class to interface.

--- plugins-new/fstab/frontend/src/app/fstab/view/fstab/devices.type.ts
+++ plugins-new/fstab/frontend/src/app/fstab/view/fstab/devices.type.ts
@@ -1,28 +1,12 @@
-export class Devices {
-
-  constructor(item: Devices) {
-    this.device = item.device;
-    this.mountpoint = item.mountpoint;
-    this.type = item.type;
-    this.options = item.options;
-    this.freq = item.freq;
-    this.passno = item.passno;
-  }
-
-  public device: string;
-  public mountpoint: string;
-  public type: string;
-  public options: string;
-  public freq: string;
-  public passno: string;
-
+export interface Devices {
+  device: string;
+  mountpoint: string;
+  type: string;
+  options: string;
+  freq: string;
+  passno: string;
 }
 
-export class Fstab {
-
-  constructor(item: Fstab) {
-    this.filesystems = item.filesystems;
-  }
-
-  public filesystems: Array<Devices>;
+export interface Fstab {
+  filesystems: Array<Devices>;
 }
