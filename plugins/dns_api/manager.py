781c0e3a4a080bb2779d4609dcf814e2d64518f5
kiarn <arnaud@kientz.me>
2023-04-23 00:34:46 +0200

    Add domain manager.

---
+++ plugins/dns_api/manager.py
@@ -0,0 +1,38 @@
+from aj.plugins.dns_api.api import ApiDnsManager
+
+class Domain:
+
+    def __init__(self, fqdn, manager):
+        self.fqdn = fqdn
+        self.mgr = manager
+        self.api = manager.api_provider
+        self.records = []
+
+    def get_records(self):
+        self.api.get_records(self)
+
+    def get(self, name):
+        try:
+            for record in self.records:
+                if record.name == name:
+                    print(record)
+        except Exception as e:
+            print(e)
+
+class DomainManager:
+
+    def __init__(self, context):
+        self.context = context
+        for provider in ApiDnsManager.all(self.context):
+            # TODO : gandi is the only one, need to prepare a config entry
+            if provider.id == 'gandi':
+                self.api_provider = provider
+                break
+        self.domains = {}
+
+    def load(self):
+        self.get_domains()
+
+    def get_domains(self):
+        for domain in self.api_provider.get_domains():
+            self.domains[domain] = Domain(domain, self)
