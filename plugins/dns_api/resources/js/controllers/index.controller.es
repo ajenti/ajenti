2d1413beaf12b43023e59449422d649899499cab
kiarn <arnaud@kientz.me>
2023-04-23 22:42:10 +0200

    Display list of records.

--- plugins/dns_api/resources/js/controllers/index.controller.es
+++ plugins/dns_api/resources/js/controllers/index.controller.es
@@ -1,7 +1,16 @@
 angular.module('ajenti.dns_api').controller('DnsAPIIndexController', function($scope, $http, pageTitle, gettext, notify) {
     pageTitle.set(gettext('DNS API'));
 
-    $http.get('/api/dns_api');
+    $http.get('/api/dns_api/domains').then((resp) => {
+        $scope.domains = resp.data;
+        $scope.active_domain = resp.data[0]; // Could not exist
+        $scope.get_records();
+    });
 
+    $scope.get_records = () => {
+        $http.get(`/api/dns_api/domain/${$scope.active_domain}/records`).then((rsp) => {
+            $scope.records = rsp.data;
+        });
+    };
 });
 
--- plugins/dns_api/views.py
+++ plugins/dns_api/views.py
@@ -1,9 +1,11 @@
 from jadi import component
+from dataclasses import asdict
 
 from aj.api.http import get, HttpPlugin
 from aj.auth import authorize
 from aj.api.endpoint import endpoint, EndpointError
 from aj.plugins.dns_api.manager import DomainManager
+
 
 @component(HttpPlugin)
 class Handler(HttpPlugin):
@@ -12,8 +14,15 @@ class Handler(HttpPlugin):
         self.mgr = DomainManager(self.context)
 
 
-    @get(r'/api/dns_api')
+    @get(r'/api/dns_api/domains')
     @endpoint(api=True)
-    def handle_api_example_dns_api(self, http_context):
+    def handle_api_dns_list_domains(self, http_context):
         self.mgr.load()
-        print(self.mgr.domains)
+        return list(self.mgr.domains.keys())
+
+    @get(r'/api/dns_api/domain/(?P<fqdn>[\w\.]+)/records')
+    @endpoint(api=True)
+    def handle_api_dns_list_records(self, http_context, fqdn):
+        self.mgr.domains[fqdn].get_records()
+        return [asdict(r) for r in self.mgr.domains[fqdn].records]
+
