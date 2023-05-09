cdb5d93be39053b6f9eae24af0e09282b4027fc1
kiarn <arnaud@kientz.me>
2023-05-09 13:14:04 +0200

    Import test ng-material.

--- plugins-new/fstab/frontend/src/app/fstab/fstab.module.ts
+++ plugins-new/fstab/frontend/src/app/fstab/fstab.module.ts
@@ -4,6 +4,8 @@ import { RouterModule } from '@angular/router';
 import { FSTAB_ROUTES } from './fstab.routes';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AjentiModule } from '@ajenti/ajenti.module';
+import { MatButtonModule } from '@angular/material/button';
+
 // import { FstabDialogComponent } from './view/fstab-dialog/fstab-dialog.component';
 
 @NgModule({
@@ -17,6 +19,7 @@ import { AjentiModule } from '@ajenti/ajenti.module';
     AjentiModule,
     RouterModule.forChild(FSTAB_ROUTES),
     DragDropModule,
+    MatButtonModule,
   ],
 })
 export class FstabModule {
