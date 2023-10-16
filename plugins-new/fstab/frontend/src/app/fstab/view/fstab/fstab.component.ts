3ca67b81ab312d556afacb34a8ddfbaa5268892e
kiarn <arnaud@kientz.me>
2023-10-16 22:05:27 +0200

    Sweep.

--- plugins-new/fstab/frontend/src/app/fstab/view/fstab/fstab.component.ts
+++ plugins-new/fstab/frontend/src/app/fstab/view/fstab/fstab.component.ts
@@ -99,34 +99,4 @@ export class FstabComponent {
     let resp = await lastValueFrom(this.httpClient.post('/api/fstab', {config: this.fstab}));
     this.notify.success('', 'Fstab successfully saved!');
   };
-
-    //   OLD CODE
-    //   public async edit(device) => {
-    //     $scope.edit_device = device;
-    //     $scope.showDetails = true;
-    // }
-    //     $scope.remove = (device) => {
-    //     messagebox.show({
-    //         text: gettext('Do you really want to permanently delete this entry?'),
-    //         positive: gettext('Delete'),
-    //         negative: gettext('Cancel')
-    //     }).then( () => {
-    //         position = $scope.fstab.indexOf(device);
-    //         $scope.fstab.splice(position, 1);
-    //         $scope.save();
-    //     })
-    // }
-    //
-    //     $scope.add = () => {
-    //     $scope.add_new = true;
-    //     $scope.edit_device = {
-    //         'device': '',
-    //         'mountpoint': '/',
-    //         'type': 'ext4',
-    //         'options': 'defaults',
-    //         'freq': '0',
-    //         'passno': '0',
-    //     };
-    //     $scope.showDetails = true;
-    // }
 }
