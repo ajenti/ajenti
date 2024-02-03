0a8f76d9b516f2f50b81c9437df586b3ec46f314
kiarn <arnaud@kientz.me>
2024-02-03 12:30:51 +0100

    Check return code.

--- plugins/passwd/views.py
+++ plugins/passwd/views.py
@@ -29,8 +29,7 @@ class Handler(HttpPlugin):
         """
 
         if os.getuid() != 0:
-            http_context.respond_forbidden()
-            return ''
+            return http_context.respond_forbidden()
 
         return [
             {
@@ -56,8 +55,13 @@ class Handler(HttpPlugin):
 
         data = http_context.json_body()
         if os.getuid() != 0 and data['users'] != self.context.identity:
-            http_context.respond_forbidden()
-            return ''
+            return http_context.respond_forbidden()
 
         p = subprocess.Popen(['chpasswd'], stdin=subprocess.PIPE)
         p.communicate(f'{data["users"]}:{data["password"]}'.encode())
+
+        if p.returncode == 1:
+            # TODO: Only a catchall, not optimal
+            return http_context.respond_bad_request()
+
+
