68ff2ed20654683c9dd781f2e65ef6f1cd539bf7
kiarn <arnaud@linuxmuster.net>
2026-03-31 12:06:47 +0200

    Check uid before lauching a pip taks.

--- plugins/plugins/tasks.py
+++ plugins/plugins/tasks.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import logging
 import subprocess
 
 from aj.plugins.core.api.tasks import Task
@@ -12,22 +14,28 @@ class InstallPlugin (Task):
         self.spec = f'ajenti.plugin.{name}=={version}'
 
     def run(self):
-        subprocess.check_output([sys.executable, '-m', 'pip', 'install', self.spec])
+        if os.getuid() == 0:
+            subprocess.check_output([sys.executable, '-m', 'pip', 'install', self.spec])
+        else:
+            logging.info("Task aborted: no sufficient privileges")
 
 
 class UpgradeAll (Task):
     name = 'Upgrading Ajenti'
 
     def run(self):
-        try:
-            if sys.executable.startswith('/opt/ajenti'):
-                # Virtualenv install
-                subprocess.check_output(['/opt/ajenti/bin/ajenti-upgrade'])
-            else:
-                # Trying with default paths
-                subprocess.check_output(['ajenti-upgrade'])
-        except FileNotFoundError as e:
-            subprocess.check_output(['/usr/local/bin/ajenti-upgrade'])
+        if os.getuid() == 0:
+            try:
+                if sys.executable.startswith('/opt/ajenti'):
+                    # Virtualenv install
+                    subprocess.check_output(['/opt/ajenti/bin/ajenti-upgrade'])
+                else:
+                    # Trying with default paths
+                    subprocess.check_output(['ajenti-upgrade'])
+            except FileNotFoundError as e:
+                subprocess.check_output(['/usr/local/bin/ajenti-upgrade'])
+        else:
+            logging.info("Task aborted: no sufficient privileges")
 
 
 class UnInstallPlugin (Task):
@@ -38,5 +46,8 @@ class UnInstallPlugin (Task):
         self.spec = f'ajenti.plugin.{name}'
 
     def run(self):
-        subprocess.check_output([sys.executable, '-m', 'pip', 'uninstall', '-y', self.spec])
+        if os.getuid() == 0:
+            subprocess.check_output([sys.executable, '-m', 'pip', 'uninstall', '-y', self.spec])
+        else:
+            logging.info("Task aborted: no sufficient privileges")
 
