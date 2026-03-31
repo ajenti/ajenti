20a4e3dcf5c038bbfaf9a2051bfdb48e7d952968
kiarn <arnaud@linuxmuster.net>
2026-03-31 12:18:07 +0200

    List allowed tasks. Needs to be more configurable.

--- plugins/core/views/tasks.py
+++ plugins/core/views/tasks.py
@@ -6,6 +6,16 @@ from aj.api.endpoint import endpoint
 from aj.plugins.core.api.tasks import TasksService
 
 
+# This need to be more configurable, in order to allow custom user's plugins
+ALLOWED_TASK_CLASSES = {
+    'aj.plugins.plugins.tasks.InstallPlugin',
+    'aj.plugins.plugins.tasks.UnInstallPlugin',
+    'aj.plugins.plugins.tasks.UpgradeAll',
+    'aj.plugins.filesystem.tasks.Transfer',
+    'aj.plugins.filesystem.tasks.Delete',
+    'aj.plugins.packages.tasks.UpdateLists',
+}
+
 @component(HttpPlugin)
 class Handler(HttpPlugin):
     def __init__(self, context):
@@ -16,12 +26,16 @@ class Handler(HttpPlugin):
     @endpoint(api=True)
     def handle_api_tasks_start(self, http_context):
         data = json.loads(http_context.body.decode())
-        modulename, clsname = data['cls'].rsplit('.', 1)
-        module = __import__(modulename, fromlist=[''])
-        cls = getattr(module, clsname)
-        task = cls(self.context, *data.get('args', []), **data.get('kwargs', {}))
-        self.service.start(task)
-        return task.id
+        cls_full_path = data['cls']
+        if cls_full_path in ALLOWED_TASK_CLASSES:
+            modulename, clsname = data['cls'].rsplit('.', 1)
+            module = __import__(modulename, fromlist=[''])
+            cls = getattr(module, clsname)
+            task = cls(self.context, *data.get('args', []), **data.get('kwargs', {}))
+            self.service.start(task)
+            return task.id
+        http_context.respond_forbidden()
+        return ''
 
     @get('/api/core/tasks/request-update')
     @endpoint(api=True)
