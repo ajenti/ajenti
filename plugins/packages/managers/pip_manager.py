8def0a66c30dfa3dc7dde8903f0a9872354c271f
kiarn <arnaud@linuxmuster.net>
2026-06-20 19:14:25 +0200

    Rewrite pypi search and sort per relevance.

--- plugins/packages/managers/pip_manager.py
+++ plugins/packages/managers/pip_manager.py
@@ -1,8 +1,7 @@
 import sys
-import importlib
 import importlib.metadata
+from concurrent.futures import ThreadPoolExecutor
 from requests import Session
-from bs4 import BeautifulSoup
 
 from jadi import component
 from aj.plugins.packages.api import PackageManager, Package
@@ -17,8 +16,13 @@ class PIPPackageManager(PackageManager):
     id = 'pip'
     name = 'PIP'
 
+    PYPI_SIMPLE_URL = 'https://pypi.org/simple/'
+    PYPI_JSON_URL = 'https://pypi.org/pypi/{name}/json'
+    MAX_RESULTS = 20
+
     def __init__(self, context):
         PackageManager.__init__(self, context)
+        self._session = Session()
 
     def __make_package_pipdist(self, dist):
         """
@@ -39,63 +43,69 @@ class PIPPackageManager(PackageManager):
         p.installed_version = dist.version
         return p
 
-    def __make_package(self, dist):
+    def __make_package(self, info, urls):
         """
-        Convert pip package object in package object.
+        Convert PyPI JSON API response into a Package object.
 
-        :param apt_package: Pip package object from apt module
-        :type apt_package: Pip package object from apt module
+        :param info: info block from PyPI JSON API response
+        :type info: dict
+        :param urls: urls block from PyPI JSON API response
+        :type urls: list
         :return: Package object
         :rtype: Package object
         """
 
         p = Package(self)
-        p.id = f'{dist["name"]}=={dist["version"]}'
-        p.name = dist['name']
-        p.version = dist['version']
-        p.description = dist['summary']
-        p.created = dist['created']
+        p.id = f'{info["name"]}=={info["version"]}'
+        p.name = info['name']
+        p.version = info['version']
+        p.description = info['summary']
+        p.created = urls[0]['upload_time'] if urls else None
         for d in importlib.metadata.distributions():
-            if d.name == p.name:
+            if d.name and d.name.lower().replace('-', '_') == info['name'].lower().replace('-', '_'):
                 p.is_installed = True
                 p.installed_version = d.version
                 p.is_upgradeable = p.installed_version != p.version
         return p
 
-    def __requests_pypi_search(self, query):
-        """
-        Launch a query on pypi search and filter the results with bs4.
-        Heavily inspired from https://github.com/victorgarric/pip_search/blob/master/pip_search/pip_search.py
+    def __fetch_pypi_metadata(self, name):
+        try:
+            resp = self._session.get(self.PYPI_JSON_URL.format(name=name), timeout=10)
+            return resp.json()
+        except Exception:
+            return None
 
-        :param query: string to search for
+    def __search_pypi(self, query):
+        """
+        Search packages on PyPI using the Simple API (PEP 691).
+
+        :param query: Search string
         :type query: string
-        :return:
-        :rtype:
+        :return: List of matching package names, capped at MAX_RESULTS
+        :rtype: list
         """
 
-        api_url = 'https://pypi.org/search/'
-        session = Session()
+        resp = self._session.get(
+            self.PYPI_SIMPLE_URL,
+            headers={'Accept': 'application/vnd.pypi.simple.v1+json'},
+            timeout=30,
+        )
+        q = query.lower()
+        matches = [
+            p['name'] for p in resp.json()['projects']
+            if q in p['name'].lower()
+        ]
 
-        packages = []
-        snippets = []
-        for page in range(1,3):
-            params = {
-                'q': query,
-                'page': page,
-            }
-            result = session.get(api_url, params=params)
+        def relevance(name):
+            n = name.lower()
+            if n == q:
+                return 0
+            if n.startswith(q):
+                return 1
+            return 2
 
-            soup = BeautifulSoup(result.text, 'html.parser')
-            snippets += soup.select('a[class*="snippet"]')
-
-        for snippet in snippets:
-            package = {}
-            for value in ["name", "version", "created"]:
-                package[value] = snippet.select_one(f'span[class*="{value}"]').text.strip()
-            package['summary'] = snippet.select_one(f'p[class*="description"]').text.strip()
-            packages.append(package)
-
-        return packages
+        matches.sort(key=relevance)
+        return matches[:self.MAX_RESULTS]
 
     def list(self, query=None):
         """
@@ -107,8 +117,12 @@ class PIPPackageManager(PackageManager):
         :rtype:Package object
         """
 
-        for dist in self.__requests_pypi_search(query):
-            yield self.__make_package(dist)
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            results = executor.map(self.__fetch_pypi_metadata, self.__search_pypi(query))
+        for data in results:
+            if not data or 'info' not in data:
+                continue
+            yield self.__make_package(data['info'], data.get('urls', []))
 
     def get_package(self, _id):
         """
