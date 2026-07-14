f07f02e0cdeeb3e528c1d5af28e27fd11623c49c
kiarn <arnaud@linuxmuster.net>
2026-07-14 23:18:46 +0200

    Check name and version of package.

--- plugins/plugins/tasks.py
+++ plugins/plugins/tasks.py
@@ -1,9 +1,20 @@
 import os
+import re
 import sys
 import logging
 import subprocess
 
 from aj.plugins.core.api.tasks import Task
+
+_SAFE_NAME = re.compile(r'^[A-Za-z0-9]([A-Za-z0-9._-]*[A-Za-z0-9])?$')
+_SAFE_VERSION = re.compile(r'^[A-Za-z0-9]([A-Za-z0-9.+_-]*[A-Za-z0-9])?$')
+
+
+def _check_spec_parts(name, version=None):
+    if not (name and _SAFE_NAME.match(name)):
+        raise ValueError(f'Refusing malformed package name {name!r}')
+    if version is not None and not (version and _SAFE_VERSION.match(version)):
+        raise ValueError(f'Refusing malformed package version {version!r}')
 
 
 class InstallPlugin (Task):
@@ -11,11 +22,16 @@ class InstallPlugin (Task):
 
     def __init__(self, context, name=None, version=None):
         Task.__init__(self, context)
+        _check_spec_parts(name, version)
         self.spec = f'ajenti.plugin.{name}=={version}'
 
     def run(self):
         if os.getuid() == 0:
-            subprocess.check_output([sys.executable, '-m', 'pip', 'install', self.spec])
+            subprocess.check_output([
+                sys.executable, '-m', 'pip', 'install',
+                '--no-deps', '--index-url', 'https://pypi.org/simple/',
+                '--', self.spec,
+            ])
         else:
             logging.info("Task aborted: no sufficient privileges")
 
@@ -43,6 +59,7 @@ class UnInstallPlugin (Task):
 
     def __init__(self, context, name=None):
         Task.__init__(self, context)
+        _check_spec_parts(name)
         self.spec = f'ajenti.plugin.{name}'
 
     def run(self):
